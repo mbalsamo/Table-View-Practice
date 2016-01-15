7fa6a437823e9f3d0a541d04946559cafae81c54
MBalsamo <student@the-source-of-the-memes.local>
2016-01-15 11:06:09 -0600

    table view editing features

--- Table View Stuffs/ViewController.swift
+++ Table View Stuffs/ViewController.swift
@@ -4,14 +4,38 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
     @IBOutlet weak var myTableView: UITableView!
-var superheros = ["Rare Pepe", "Wonderwoman", "Batman", "Flash", "Scruba Steve", "Aquaman"]
-var realNames = ["Master N8", "Diana", "Bruce Wayne", "Barry Allen", "Steve", "Arthur Curry"]
+var superheros = ["Superman", "Wonderwoman", "Batman", "Flash", "Scruba Steve", "Aquaman"]
+var realNames = ["Bruce Wayne", "Diana", "Bruce Wayne", "Barry Allen", "Steve", "Arthur Curry"]
     override func viewDidLoad()
     {
         super.viewDidLoad()
         //setting datasource and delagate
         myTableView.dataSource = self
         myTableView.delegate = self
+    }
+    @IBAction func addButtonTapped(sender: UIBarButtonItem)
+    {
+        let myAlert = UIAlertController(title: "Add Super Hero", message: nil, preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        myAlert.addAction(cancelAction)
+        
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (addAction) -> Void in
+            let superheroNameTextField = myAlert.textFields![0] as UITextField
+            let aliasTextField = myAlert.textFields![1]  as UITextField //basically connecting the text field
+            self.superheros.append(superheroNameTextField.text!)
+            self.realNames.append(aliasTextField.text!)
+            self.myTableView.reloadData()
+        }
+        myAlert.addAction(addAction)
+        
+        myAlert.addTextFieldWithConfigurationHandler { (superheroTextField) -> Void in
+            superheroTextField.placeholder = "Add Superhero Name"
+        }
+        myAlert.addTextFieldWithConfigurationHandler { (aliasTextField) -> Void in
+            aliasTextField.placeholder = "Add Real Name"
+        }
+        //this is what presents the alert
+        self.presentViewController(myAlert, animated: true, completion: nil)
     }
     
     //creates a cell that will store data
@@ -28,8 +52,19 @@ var realNames = ["Master N8", "Diana", "Bruce Wayne", "Barry Allen", "Steve", "A
     {
         return superheros.count
     }
-    
+    //allows you to delete rows from table view
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == .Delete
+        {
+            superheros.removeAtIndex(indexPath.row)
+            realNames.removeAtIndex(indexPath.row)
+            myTableView.reloadData()
+        }
+
+    }
 }
+
 
 
 
